44bf62ac0b9cc0b8daac4d15337ebce3ff7caadc
Doan Nguyen <nguyentrongdoan.0@gmail.com>
2022-02-04 16:02:11 -0800

    merge fixes from FW2.1 branch

--- roboteq_controller/src/roboteq_controller_node.cpp
+++ roboteq_controller/src/roboteq_controller_node.cpp
@@ -344,8 +344,8 @@ void RoboteqDriver::queryCallback(){
 				}
 			}
 
-			if (fields_H[0] == "H"){
-				for (int i = 0; i < query_pub_.size(); ++i){
+			if (fields_H.size() > 0 && fields_H[0] == "H"){
+				for (int i = 0; i < fields_H.size() - 1; ++i){
 					std::vector<std::string> sub_fields_H;
 					boost::split(sub_fields_H, fields_H[i + 1], boost::algorithm::is_any_of(":"));
 					
